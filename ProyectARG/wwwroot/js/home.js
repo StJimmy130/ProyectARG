a537d2a13e42230c0a45eff9bb124491b9e48324
Davito <davidracca02@gmail.com>
2024-09-13 16:15:34 -0300

    fix notificaciones

--- ProyectARG/wwwroot/js/home.js
+++ ProyectARG/wwwroot/js/home.js
@@ -310,3 +310,22 @@ function syncInput(id) {
   // Sincronizar el valor del input de número con el range
   input.value = range.value;
 }
+
+// js code to toggle search box
+searchToggle.addEventListener("click", () => {
+  searchToggle.classList.toggle("active");
+});
+// Write your JavaScript code.
+//   js code to toggle sidebar
+sidebarOpen.addEventListener("click", () => {
+  nav.classList.add("active");
+});
+body.addEventListener("click", (e) => {
+  let clickedElm = e.target;
+  if (
+      !clickedElm.classList.contains("sidebarOpen") &&
+      !clickedElm.classList.contains("menu")
+  ) {
+      nav.classList.remove("active");
+  }
+});
--- ProyectARG/wwwroot/js/site.js
+++ ProyectARG/wwwroot/js/site.js
@@ -19,25 +19,9 @@ modeToggle.addEventListener("click", () => {
         localStorage.setItem("mode", "dark-mode");
     }
 });
-// js code to toggle search box
-searchToggle.addEventListener("click", () => {
-    searchToggle.classList.toggle("active");
-});
 
-// Write your JavaScript code.
-//   js code to toggle sidebar
-sidebarOpen.addEventListener("click", () => {
-    nav.classList.add("active");
-});
-body.addEventListener("click", (e) => {
-    let clickedElm = e.target;
-    if (
-        !clickedElm.classList.contains("sidebarOpen") &&
-        !clickedElm.classList.contains("menu")
-    ) {
-        nav.classList.remove("active");
-    }
-});
+
+
 
 // Función para mostrar la pantalla de carga
 function showLoadingScreen() {
