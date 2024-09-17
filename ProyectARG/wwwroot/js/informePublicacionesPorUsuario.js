f7dc09cf90fef89bca62b28a34fb9bc99c131371
StJimmy130 <nicolasmaidana811@gmail.com>
2024-09-17 18:30:47 -0300

    Imprimir

--- ProyectARG/wwwroot/js/informePublicacionesPorUsuario.js
+++ ProyectARG/wwwroot/js/informePublicacionesPorUsuario.js
@@ -45,3 +45,62 @@ function ListadoInformePorLugar() {
         }
     });
 }
+
+function Imprimir() {
+    var doc = new jsPDF('l', 'mm', [297, 210]);
+  
+    var totalPagesExp = "{total_pages_count_string}";
+    var pageContent = function (data) {
+        var pageHeight = doc.internal.pageSize.height || doc.internal.pageSize.getHeight();
+        var pageWidth = doc.internal.pageSize.width || doc.internal.pageSize.getWidth();
+  
+        // HEADER
+        doc.setFontSize(14);
+        doc.setFont("helvetica", "bold");
+        doc.text("Informe de publicaciones por usuario", 15, 10);
+  
+        // FOOTER
+        var str = "Página " + data.pageCount;
+        if (typeof doc.putTotalPages == "function") {
+            str = str + " de " + totalPagesExp;
+        }
+  
+        doc.setLineWidth(8);
+        doc.setDrawColor(1, 1, 1);
+        doc.line(14, pageHeight - 11, 196, pageHeight - 11);
+  
+        doc.setFontSize(10);
+        doc.setFontStyle("bold");
+        doc.text(str, 17, pageHeight - 10);
+    };
+  
+    var elem = document.getElementById("TablaPorUsuario");
+    var res = doc.autoTableHtmlToJson(elem);
+    doc.autoTable(res.columns, res.data, {
+        addPageContent: pageContent,
+        theme: 'grid',
+        styles: { fillColor: [0, 143, 81], halign: "center" },
+        columnStyles: {
+            0: { halign: "left", fillColor: [255, 255, 255], fontSize: 7 }, 
+            1: { halign: "left", fillColor: [255, 255, 255], fontSize: 7 },
+            2: { halign: "right", fillColor: [255, 255, 255], fontSize: 7, cellWidth: 100 },
+            3: { halign: "left", fillColor: [255, 255, 255], fontSize: 7 },
+            4: { halign: "left", fillColor: [255, 255, 255], fontSize: 7 },
+            5: { halign: "left", fillColor: [255, 255, 255], fontSize: 7 },
+            6: { halign: "left", fillColor: [255, 255, 255], fontSize: 7 },
+        },
+        margin: { top: 20 },
+    });
+  
+    if (typeof doc.putTotalPages === "function") {
+        doc.putTotalPages(totalPagesExp);
+    }
+  
+    var string = doc.output("datauristring");
+    var iframe = "<iframe width='100%' height='100%' src='" + string + "'></iframe>";
+  
+    var x = window.open();
+    x.document.open();
+    x.document.write(iframe);
+    x.document.close();
+}
