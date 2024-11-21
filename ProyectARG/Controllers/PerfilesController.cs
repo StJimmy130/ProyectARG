f8149611c1f5931f8fe41e4e85114dd3f2bf321a
StJimmy130 <nicolasmaidana811@gmail.com>
2024-11-20 21:19:21 -0300

    Mis Publicaciones

--- ProyectARG/Controllers/PerfilesController.cs
+++ ProyectARG/Controllers/PerfilesController.cs
@@ -11,7 +11,7 @@ using System.Text.RegularExpressions;
 
 namespace ProyectARG.Controllers;
 
-
+[Authorize]
 public class PerfilesController : Controller
 {
     private ApplicationDbContext _context;
